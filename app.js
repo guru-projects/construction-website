12c8a13c94ce74b088a63f40e6d98735c0d078ce
guru <guru85111@gmail.com>
2023-09-05 16:13:13 +0530

    bugs

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 // mongodb+srv://guru:FfotG2CSTqJ2lJvd @cluster0.evtv81e.mongodb.net/
 // mongodb://127.0.0.1:27017/constructionDB
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb+srv://guru:FfotG2CSTqJ2lJvd@cluster0.evtv81e.mongodb.net/"); 
+mongoose.connect("mongodb+srv://guru:FfotG2CSTqJ2lJvd @cluster0.evtv81e.mongodb.net/"); 
 console.log(" Connected to MONGODB... ");
  
 const userSchema = new mongoose.Schema({
@@ -58,28 +58,69 @@ app.get("/contact", function(req, res) {
     res.render("contact");
 })
 app.get("/1", function(req, res) {
-    User.find().then(users => {
+    User1.find().then(users => {
         res.render("blogPages/1", {
             users: users
             });
     })
 });
 app.get("/2", function(req, res) {
-    res.render("blogPages/2");
+    User2.find().then(users => {
+        res.render("blogPages/1", {
+            users: users
+            });
+    })
+});
+app.get("/3", function(req, res) {
+    User3.find().then(users => {
+        res.render("blogPages/3", {
+            users: users
+            });
+    })
 });
 //Creating a mongoose model based on this Schema :
  
-const User = mongoose.model("User", userSchema);
+const User1 = mongoose.model("User1", userSchema);
+const User2 = mongoose.model("User2", userSchema);
+const User3 = mongoose.model("User3", userSchema);
 
 app.post("/1", function(req, res) {
-    console.log("user");
-    const user = new User({ 
+
+    const user = new User1({ 
         name: req.body.name,
         content : req.body.content
     })
     user.save().then(() => {
-        console.log('User added to DB.');
+        console.log('User1 added to DB.');
         res.redirect("/1");
+      })
+      .catch(err => {
+        res.status(400).send("Unable to save User to database.");
+      });
+});
+app.post("/2", function(req, res) {
+    
+    const user = new User2({ 
+        name: req.body.name,
+        content : req.body.content
+    })
+    user.save().then(() => {
+        console.log('User2 added to DB.');
+        res.redirect("/2");
+    })
+    .catch(err => {
+        res.status(400).send("Unable to save User to database.");
+    });
+});
+app.post("/3", function(req, res) {
+
+    const user = new User3({ 
+        name: req.body.name,
+        content : req.body.content
+    })
+    user.save().then(() => {
+        console.log('User3 added to DB.');
+        res.redirect("/3");
       })
       .catch(err => {
         res.status(400).send("Unable to save User to database.");
